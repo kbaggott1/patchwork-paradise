7be56053aa55c83d903c33e25686cac9b44b1619
kbaggott1 <kevin.baggott1@gmail.com>
2023-12-17 16:12:25 -0500

    change player money start

--- Patchwork-Paradise/src/states/game/TitleScreenState.js
+++ Patchwork-Paradise/src/states/game/TitleScreenState.js
@@ -69,7 +69,7 @@ export default class TitleScreenState extends State {
 	} 
 
 	initializeGame() {
-		const money = Number(JSON.parse(localStorage.getItem("money")) ?? 500);
+		const money = Number(JSON.parse(localStorage.getItem("money")) ?? 10);
 		//const player = new Player({ position: TownMap.PLAYER_SPAWN_POSITION}, new Outfit(Clothes.Basic,  ClothesColor.Red, Clothes.Pants, ClothesColor.Black, Clothes.Shoes, ClothesColor.Black, Hair.Gentleman, HairColor.Brown));
 		const player = new Player({ position: {x: FarmMap.PLAYER_SPAWN_POSITION.x, y: TownMap.PLAYER_SPAWN_POSITION.y } }, new Outfit(Clothes.Basic,  ClothesColor.Red, Clothes.Pants, ClothesColor.Black, Clothes.Shoes, ClothesColor.Black, Hair.Gentleman, HairColor.Brown), money);
 		this.loadObjectStore();
